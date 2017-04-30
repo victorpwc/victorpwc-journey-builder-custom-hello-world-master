dd66e58d0c8a63a798e9623b28ab0873f3f5654d
victorpwc <VLOPEZFUEN001@es.ema.ad.pwcinternal.com>
2017-05-01 00:25:33 +0200

    First Commit

---
+++ public/ixn/activities/hello-world/config.js
@@ -0,0 +1,57 @@
+define([], function(){      
+    return {
+        "icon": "images/jb-icon.jpg",
+        "iconSmall": "images/jb-icon.jpg", 
+        "key": "jb-hello-world-activity-vlf",
+        "partnerApiObjectTypeId": "IXN.CustomActivity.REST",
+        "lang": {
+            "en-US": {        
+                "name": "Hello World Activity 2",
+                "description": "Activity simply posts the data into an array for display on the App's home page."
+            }
+        },
+        "category": "messaging",
+        "version": "1.0",
+        "apiVersion": "1.0",
+       "execute": {
+            "uri": "https://nibsmuc14-demo.herokuapp.com/ixn/activities/hello-world/execute/",
+			"inArguments": [],
+			"outArguments": [],
+            "verb": "POST",
+			"body": "",
+            "format": "json",
+            "useJwt": false,
+            "timeout": 3000
+		},
+        "save": {
+            "uri": "https://nibsmuc14-demo.herokuapp.com/ixn/activities/hello-world/save/",
+			"verb": "POST",
+			"body": "",
+            "format": "json",
+            "useJwt": false,
+            "timeout": 3000
+        },
+        "publish": {
+            "uri": "https://nibsmuc14-demo.herokuapp.com/ixn/activities/hello-world/publish/",
+            "verb": "POST",
+			"body": "",
+            "format": "json",
+            "useJwt": false,
+            "timeout": 3000
+        },
+        "validate": {
+            "uri": "https://nibsmuc14-demo.herokuapp.com/ixn/activities/hello-world/validate/",
+            "verb": "POST",
+			"body": "",
+            "format": "json",
+            "useJwt": false,
+            "timeout": 3000
+        },
+
+        "edit": {
+            "uri": "https://nibsmuc14-demo.herokuapp.com/ixn/activities/hello-world/",
+            "height": 400,
+            "width": 500
+        }
+};
+});
